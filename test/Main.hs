743457d204d2f74b715212db4121a3d3e3cc4bd4
Mrjt Jmn <meritamen@sdf.org>
2025-07-07 02:01:44 +0800

    Add unit tests for OpenProduct

--- test/Main.hs
+++ test/Main.hs
@@ -3,9 +3,10 @@ module Main (main) where
 import Test.Tasty
 import Test.Tasty.Hspec
 
+import ProductSpecs
 import SumSpecs
 
 main :: IO ()
 main = do
-  specs <- concat <$> mapM testSpecs [sumSpecs]
+  specs <- concat <$> mapM testSpecs [productSpecs, sumSpecs]
   defaultMain $ testGroup "All Tests" [testGroup "Specs" specs]
---
+++ test/ProductSpecs.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedLabels #-}
+
+module ProductSpecs where
+
+import Data.Extensible.Product
+import Test.Hspec
+
+productSpecs :: Spec
+productSpecs = do
+  describe "OpenProduct" $ do
+    it "insert and get Int" $
+      get #IntKey (insert #DoubleKey (Just @Double 8.964) (insert #IntKey (Just 10) nil)) `shouldBe` Just @Int 10
+    it "insert and update double" $ do
+      get #DoubleKey (update #DoubleKey (Just @Double 2.952)(insert #DoubleKey (Just @Double 8.964) (insert #IntKey (Just @Int 10) nil))) `shouldBe` Just @Double 2.952
+    it "insert, delete, insert, and get Int" $ do
+      get #IntKey (insert #IntKey (Just 20) (delete #IntKey (insert #IntKey (Just @Int 10) nil))) `shouldBe` Just @Int 20
+    it "upsert and get Int" $
+      get #IntKey (upsert #DoubleKey (Just @Double 8.964) (insert #IntKey (Just 10) nil)) `shouldBe` Just @Int 10
